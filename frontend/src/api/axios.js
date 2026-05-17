1d5cb6fa523151a5a93f17ee687fce4c334ba909
Navaneeth <tammineedinavaneeth2@gmail.com>
2026-05-18 01:55:29 +0530

    fix: Update API baseURL to use secure CloudFront proxy to resolve Mixed Content Block

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://a44873505701344cba71f72d3e0dc774-1621922902.ap-south-1.elb.amazonaws.com/api'
+  baseURL: 'https://dgeb48f0ffhmi.cloudfront.net/api'
 });
 
 API.interceptors.request.use((req) => {
